430a4e47f3eb9d893497dd95adfe57c4f5743a20
ChristianAlexa <cjalexa@gmail.com>
2021-07-25 19:17:06 -0400

    grid - gut boiler and init homepage

---
+++ src/components/GridHeader/GridHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function GridHeader() {
+  return (
+    <div>
+      Grid Header!
+      <a href="#">archive</a>
+      <a href="#">logout</a>
+    </div>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GridHeader from '../components/GridHeader';
+
+export default function HomePage() {
+  return (
+    <div>
+      <GridHeader />
+      Grid HomePage!
+    </div>
+  );
+}
