b38f93a0ec370c8d2d883b369758552ddc5ca326
ChristianAlexa <cjalexa@gmail.com>
2021-07-25 23:55:12 -0400

    grid - mv HomePage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Grid HomePage text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const gridTextElement = screen.getByText(/Grid HomePage/i);
+  expect(gridTextElement).toBeInTheDocument();
 });
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayoutHeader from '../../components/PageLayoutHeader';
+
+export default function HomePage() {
+  return (
+    <div id='homepage'>
+      <PageLayoutHeader />
+      Grid HomePage!
+    </div>
+  );
+}
